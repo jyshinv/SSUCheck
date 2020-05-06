fffe6822c451da261cb4a197dac7614a4c340581
신지윤 <sjy1218vvv@gmail.com>
2020-05-07 01:46:37 +0900

    Menu화면 레이아웃 완성

--- app/src/main/java/org/playpang/ssucheck/C_Menu.java
+++ app/src/main/java/org/playpang/ssucheck/C_Menu.java
@@ -1,13 +1,18 @@
 package org.playpang.ssucheck;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,70 +25,29 @@ public class C_Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c__menu);
 
-        //타이틀 바 내용
+        //타이틀바 색상 변경
         ActionBar bar = getSupportActionBar();
-        bar.setTitle("숭실대학교 안면인식 출결 시스템~");
-
-        //현재 듣고 있는 과목 버튼 누르면
-        TextView tv1 = findViewById(R.id.c_menu_currentsubject);
-        tv1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),CA_CurrentSubject.class);
-                startActivity(intent);
-            }
-        });
-
-        //실시간 출결 현황 누르면
-        TextView tv2 = findViewById(R.id.c_menu_realtimecheck);
-        tv2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),CB_RealTimeCheck.class);
-                startActivity(intent);
-            }
-        });
+        bar.hide();
 
 
-        //내 출결 현황 누르면
-        TextView tv3 = findViewById(R.id.c_menu_mycheck);
-        tv3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),CC_MyCheck.class);
-                startActivity(intent);
-            }
-        });
 
 
-        //시간표 조회 누르면
-        //화면 전환
-//        TextView tv4 = findViewById(R.id.c_menu_timetable);
-//        tv4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(),CD_TimeTable.class);
-//                startActivity(intent);
-//            }
-//        });
 
-        //시간표 조회 누르면
-        //인터넷으로 연결
-        TextView tv4 = findViewById(R.id.c_menu_timetable);
-        tv4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),CD_TimeTableWebView.class);
-                startActivity(intent);
-            }
-        });
+
+
+
+
+
+
 
 
 
     }
 
-    long pressTime;
 
+
+    //2번 누르면 종료되게 만드는 코드
+    long pressTime;
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
@@ -95,29 +59,29 @@ public class C_Menu extends AppCompatActivity {
         pressTime = System.currentTimeMillis();
     }
 
-    //액션버튼 메뉴 액션바에 집어 넣기
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //return super.onCreateOptionsMenu(search_menu);
-        getMenuInflater().inflate(R.menu.setting_menu,menu);
-        return true;
-    }
-
-    //액션버튼을 클릭했을 때의 동작
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if(id == R.id.currentsubject){
-            Intent intent = new Intent(getApplicationContext(),CA_CurrentSubject.class);
-            startActivity(intent);
-           return true;
-//        }else if(id == R.id.logout){
-//            Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
-//            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }
+//    //액션버튼 메뉴 액션바에 집어 넣기
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        //return super.onCreateOptionsMenu(search_menu);
+//        getMenuInflater().inflate(R.menu.setting_menu,menu);
+//        return true;
+//    }
+//
+//    //액션버튼을 클릭했을 때의 동작
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if(id == R.id.currentsubject){
+//            Intent intent = new Intent(getApplicationContext(),CA_CurrentSubject.class);
+//            startActivity(intent);
+//           return true;
+////        }else if(id == R.id.logout){
+////            Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+////            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//
+//    }
 }
