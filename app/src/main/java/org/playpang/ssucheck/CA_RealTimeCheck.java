4d58221fe1feefac74d3ce580bfdcbef651f5c3e
신지윤 <sjy1218vvv@gmail.com>
2020-05-07 15:32:23 +0900

    과목 리스트 어댑터로 구현 완료, 파이어베이스 연동 전

--- app/src/main/java/org/playpang/ssucheck/CA_RealTimeCheck.java
+++ app/src/main/java/org/playpang/ssucheck/CA_RealTimeCheck.java
@@ -3,15 +3,40 @@ package org.playpang.ssucheck;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.playpang.ssucheck.adapter.CA_RealTimeCheckAdapter;
 
 public class CA_RealTimeCheck extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ca__real_time_check);
 
+        //액션바의 제목을 바꿈
         ActionBar bar = getSupportActionBar();
         bar.setTitle("실시간 강의 출결");
+
+        //어댑터 생성
+        ListAdapter adapter = new CA_RealTimeCheckAdapter();
+
+        //리스트 뷰 참조 및 Adapter 달기
+        ListView lv =findViewById(R.id.ca_real_time_check_lv);
+        lv.setAdapter(adapter);
+
+        ((CA_RealTimeCheckAdapter) adapter).addItem("데이터베이스(온&오프) (가,나,다,라)","Database");
+        ((CA_RealTimeCheckAdapter) adapter).addItem("컴퓨터구조(나)[214455202]","Computer Architecture");
+        ((CA_RealTimeCheckAdapter) adapter).addItem("디지털미디어원리및실습(가)[2154266025]","Digital Media Principles and practice");
+        ((CA_RealTimeCheckAdapter) adapter).addItem("컴퓨터프로그래밍(다)[213356026]","Computer Programming");
+        ((CA_RealTimeCheckAdapter) adapter).addItem("졸업프로젝트(가)(온&오프)[21589906]","Thesis Seminar");
+
+
+
+
     }
 }
---
+++ app/src/main/java/org/playpang/ssucheck/data/SubjectNameItem.java
@@ -0,0 +1,22 @@
+package org.playpang.ssucheck.data;
+
+public class SubjectNameItem {
+    String korSubject;
+    String engSubject;
+
+    public String getKorSubject() {
+        return korSubject;
+    }
+
+    public String getEngSubject() {
+        return engSubject;
+    }
+
+    public void setKorSubject(String korSubject) {
+        this.korSubject = korSubject;
+    }
+
+    public void setEngSubject(String engSubject) {
+        this.engSubject = engSubject;
+    }
+}
